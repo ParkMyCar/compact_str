6c42dc617e05c0ad47ce7ea119a1574952a6ba54
Parker Timmerman <parker@parkertimmerman.com>
2022-01-08 13:58:03 -0500

    add bytes example

---
+++ examples/bytes/src/main.rs
@@ -0,0 +1,15 @@
+use compact_str::CompactStr;
+use std::io::Cursor;
+
+fn main() {
+    let word = "hello world!";
+
+    // Cursor<&[u8]> is `bytes::Buf`
+    let mut buf = Cursor::new(word.as_bytes());
+    // `from_utf8_buf(...)` can fail, if the provided buffer is not valid UTF-8
+    let compact_str = CompactStr::from_utf8_buf(&mut buf).expect("valid utf-8");
+
+    assert_eq!(compact_str, word);
+
+    println!("{}", compact_str);
+}

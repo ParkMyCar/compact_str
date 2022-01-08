891ec917d2d2a13d91513cd2d810d54c6c971975
Parker Timmerman <parker@parkertimmerman.com>
2022-01-08 14:20:57 -0500

    add doc tests for the bytes APIs

--- compact_str/src/features/bytes.rs
+++ compact_str/src/features/bytes.rs
@@ -9,12 +9,57 @@ use crate::{
 
 impl CompactStr {
     /// Converts a buffer of bytes to a `CompactStr`
+    ///
+    /// # Examples
+    /// ### Basic usage
+    /// ```
+    /// # use compact_str::CompactStr;
+    /// # use std::collections::VecDeque;
+    ///
+    /// // `bytes::Buf` is implemented for `VecDeque<u8>`
+    /// let mut sparkle_heart = VecDeque::from([240, 159, 146, 150]);
+    /// // We know these bytes are valid, so we can `.unwrap()` or `.expect(...)` here
+    /// let compact_str = CompactStr::from_utf8_buf(&mut sparkle_heart).expect("valid utf-8");
+    ///
+    /// assert_eq!(compact_str, "💖");
+    /// ```
+    ///
+    /// ### With invalid/non-UTF8 bytes
+    /// ```
+    /// # use compact_str::CompactStr;
+    /// # use std::io;
+    ///
+    /// // `bytes::Buf` is implemented for `std::io::Cursor<&[u8]>`
+    /// let mut invalid = io::Cursor::new(&[0, 159]);
+    ///
+    /// // The provided buffer is invalid, so trying to create a `ComapctStr` will fail
+    /// assert!(CompactStr::from_utf8_buf(&mut invalid).is_err());
+    /// ```
     pub fn from_utf8_buf<B: Buf>(buf: &mut B) -> Result<Self, Utf8Error> {
         Repr::from_utf8_buf(buf).map(|repr| CompactStr { repr })
     }
 
     /// Converts a buffer of bytes to a `CompactStr`, without checking that the provided buffer is
     /// valid UTF-8.
+    ///
+    /// # Saftey
+    /// This function is unsafe because it does not check that the provided bytes are valid UTF-8. If
+    /// this constraint is violated, it may cause memory unsafety issues with futures uses of the
+    /// `ComapctStr`, as the rest of the library assumes that `CompactStr`s are valid UTF-8
+    ///
+    /// # Examples
+    /// ```
+    /// # use compact_str::CompactStr;
+    /// use std::io::Cursor;
+    ///
+    /// let word = "hello world";
+    /// // `bytes::Buf` is implemented for `Cursor<&[u8]>`
+    /// let mut buffer = Cursor::new(word.as_bytes());
+    ///
+    /// let compact_str = unsafe { CompactStr::from_utf8_buf_unchecked(&mut buffer) };
+    ///
+    /// assert_eq!(compact_str, word);
+    /// ```
     pub unsafe fn from_utf8_buf_unchecked<B: Buf>(buf: &mut B) -> Self {
         let repr = Repr::from_utf8_buf_unchecked(buf);
         CompactStr { repr }

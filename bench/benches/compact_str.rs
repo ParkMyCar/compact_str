b788a8e4c67d77f2392b19357527f1c251aa87b9
CAD97 <cad97@cad97.com>
2022-03-31 11:59:45 -0500

    Improve bench hygeine somewhat

--- bench/benches/compact_str.rs
+++ bench/benches/compact_str.rs
@@ -2,33 +2,41 @@ use compact_str::CompactStr;
 use criterion::{
     criterion_group,
     criterion_main,
+    BenchmarkId,
     Criterion,
 };
 
-fn empty(c: &mut Criterion) {
-    let word = "";
-    c.bench_function("empty", |b| b.iter(|| CompactStr::new(word)));
+fn bench_new(c: &mut Criterion) {
+    c.bench_with_input(
+        BenchmarkId::new("CompactStr::new", "0 chars"),
+        &"",
+        |b, word| b.iter(|| CompactStr::new(word)),
+    );
+
+    c.bench_with_input(
+        BenchmarkId::new("CompactStr::new", "16 chars"),
+        &"im sixteen chars",
+        |b, word| b.iter(|| CompactStr::new(word)),
+    );
+
+    c.bench_with_input(
+        BenchmarkId::new("CompactStr::new", "24 chars"),
+        &"i am twenty four chars!!",
+        |b, word| b.iter(|| CompactStr::new(word)),
+    );
+
+    c.bench_with_input(
+        BenchmarkId::new("CompactStr::new", "59 chars"),
+        &"I am a very long string that will get allocated on the heap",
+        |b, word| b.iter(|| CompactStr::new(word)),
+    );
+
+    c.bench_with_input(
+        BenchmarkId::new("String::new", "59 chars"),
+        &"I am a very long string that will get allocated on the heap",
+        |b, &word| b.iter(|| String::from(word)),
+    );
 }
 
-fn inline(c: &mut Criterion) {
-    let word = "im sixteen chars";
-    c.bench_function("inline", |b| b.iter(|| CompactStr::new(word)));
-}
-
-fn packed(c: &mut Criterion) {
-    let word = "i am twenty four chars!!";
-    c.bench_function("packed", |b| b.iter(|| CompactStr::new(word)));
-}
-
-fn heap(c: &mut Criterion) {
-    let word = "I am a very long string that will get allocated on the heap";
-    c.bench_function("heap", |b| b.iter(|| CompactStr::new(word)));
-}
-
-fn std_string(c: &mut Criterion) {
-    let word = "I am a very long string that will get allocated on the heap";
-    c.bench_function("std_string", |b| b.iter(|| String::from(word)));
-}
-
-criterion_group!(compact_str, empty, inline, packed, heap, std_string,);
+criterion_group!(compact_str, bench_new);
 criterion_main!(compact_str);

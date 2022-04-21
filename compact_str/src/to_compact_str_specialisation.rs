97da81cd38fe42aa2a9433239f6d4c1145e8f971
Jiahao XU <Jiahao_XU@outlook.com>
2022-04-21 15:41:00 +1000

    Optimize `bool` spec for `ToCompactStr`
    Use `CompactStr::new_inline` instead of `CompactStr::new`
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- compact_str/src/to_compact_str_specialisation.rs
+++ compact_str/src/to_compact_str_specialisation.rs
@@ -15,7 +15,11 @@ pub(super) fn to_compact_str_specialised<T>(val: &T) -> Option<CompactStr> {
     }
 
     if let Ok(boolean) = cast!(val, &bool) {
-        Some(CompactStr::new(if *boolean { "true" } else { "false" }))
+        Some(CompactStr::new_inline(if *boolean {
+            "true"
+        } else {
+            "false"
+        }))
     } else if let Ok(string) = cast!(val, &String) {
         Some(CompactStr::new(&*string))
     } else if let Ok(character) = cast!(val, &char) {

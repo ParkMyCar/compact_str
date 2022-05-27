b0d94d427d6ff8285758d968005a397be8caa897
Parker Timmerman <parker@parkertimmerman.com>
2022-05-27 14:20:21 -0400

    add comment explaining why f32 tests are disabled for some platforms

--- compact_str/src/repr/traits.rs
+++ compact_str/src/repr/traits.rs
@@ -67,6 +67,47 @@ mod tests {
     }
 
     #[test]
+    fn test_into_repr_f64_sanity() {
+        let vals = [
+            f64::MIN,
+            f64::MIN_POSITIVE,
+            f64::MAX,
+            f64::NEG_INFINITY,
+            f64::INFINITY,
+        ];
+
+        for x in &vals {
+            let repr = f64::into_repr(*x);
+            let roundtrip = repr.as_str().parse::<f64>().unwrap();
+
+            assert_eq!(*x, roundtrip);
+        }
+    }
+
+    #[test]
+    fn test_into_repr_f64_nan() {
+        let repr = f64::into_repr(f64::NAN);
+        let roundtrip = repr.as_str().parse::<f64>().unwrap();
+        assert!(roundtrip.is_nan());
+    }
+
+    #[proptest]
+    #[cfg_attr(miri, ignore)]
+    fn test_into_repr_f64(val: f64) {
+        let repr = f64::into_repr(val);
+        let roundtrip = repr.as_str().parse::<f64>().unwrap();
+
+        // Note: The formatting of floats by `ryu` sometimes differs from that of `std`, so instead
+        // of asserting equality with `std` we just make sure the value roundtrips
+
+        prop_assert_eq!(val, roundtrip);
+    }
+
+
+    // `f32` formatting is broken on powerpc64le, not only in `ryu` but also `std`
+    //
+    // See: https://github.com/rust-lang/rust/issues/96306
+    #[test]
     #[cfg_attr(all(target_arch = "powerpc64", target_pointer_width = "64"), ignore)]
     fn test_into_repr_f32_sanity() {
         let vals = [
@@ -99,43 +140,6 @@ mod tests {
     fn test_into_repr_f32(val: f32) {
         let repr = f32::into_repr(val);
         let roundtrip = repr.as_str().parse::<f32>().unwrap();
-
-        // Note: The formatting of floats by `ryu` sometimes differs from that of `std`, so instead
-        // of asserting equality with `std` we just make sure the value roundtrips
-
-        prop_assert_eq!(val, roundtrip);
-    }
-
-    #[test]
-    fn test_into_repr_f64_sanity() {
-        let vals = [
-            f64::MIN,
-            f64::MIN_POSITIVE,
-            f64::MAX,
-            f64::NEG_INFINITY,
-            f64::INFINITY,
-        ];
-
-        for x in &vals {
-            let repr = f64::into_repr(*x);
-            let roundtrip = repr.as_str().parse::<f64>().unwrap();
-
-            assert_eq!(*x, roundtrip);
-        }
-    }
-
-    #[test]
-    fn test_into_repr_f64_nan() {
-        let repr = f64::into_repr(f64::NAN);
-        let roundtrip = repr.as_str().parse::<f64>().unwrap();
-        assert!(roundtrip.is_nan());
-    }
-
-    #[proptest]
-    #[cfg_attr(miri, ignore)]
-    fn test_into_repr_f64(val: f64) {
-        let repr = f64::into_repr(val);
-        let roundtrip = repr.as_str().parse::<f64>().unwrap();
 
         // Note: The formatting of floats by `ryu` sometimes differs from that of `std`, so instead
         // of asserting equality with `std` we just make sure the value roundtrips

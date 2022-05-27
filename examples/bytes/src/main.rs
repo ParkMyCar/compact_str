55460f108e14437a46ca194c144055650bb913d3
Parker Timmerman <parker@parkertimmerman.com>
2022-05-27 19:37:36 -0400

    branch start, update examples to v0.4

--- examples/bytes/src/main.rs
+++ examples/bytes/src/main.rs
@@ -1,6 +1,6 @@
 use std::io::Cursor;
 
-use compact_str::CompactStr;
+use compact_str::CompactString;
 
 fn main() {
     let word = "hello world!";
@@ -8,7 +8,7 @@ fn main() {
     // Cursor<&[u8]> is `bytes::Buf`
     let mut buf = Cursor::new(word.as_bytes());
     // `from_utf8_buf(...)` can fail, if the provided buffer is not valid UTF-8
-    let compact_str = CompactStr::from_utf8_buf(&mut buf).expect("valid utf-8");
+    let compact_str = CompactString::from_utf8_buf(&mut buf).expect("valid utf-8");
 
     assert_eq!(compact_str, word);
 

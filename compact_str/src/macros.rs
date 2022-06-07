c0c4489f91ccbcd4976f7a2d44583b231e427b6c
Jiahao XU <Jiahao_XU@outlook.com>
2022-06-07 22:04:27 +1000

    Update doc of `format_compact!`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- compact_str/src/macros.rs
+++ compact_str/src/macros.rs
@@ -1,3 +1,29 @@
+/// Creates a String using interpolation of runtime expressions.
+///
+/// The first argument `format_compact!` receives is a format string.
+/// This must be a string literal.
+/// The power of the formatting string is in the `{}`s contained.
+///
+/// Additional parameters passed to `format_compact!` replace the `{}`s within
+/// the formatting string in the order given unless named or
+/// positional parameters are used; see [`std::fmt`] for more information.
+///
+/// A common use for `format_compact!` is concatenation and interpolation
+/// of strings.
+/// The same convention is used with [`print!`] and [`write!`] macros,
+/// depending on the intended destination of the string.
+///
+/// To convert a single value to a string, use the
+/// [`ToCompactString::to_compact_string`] method, which uses
+/// the [`Display`] formatting trait.
+///
+/// # Panics
+///
+/// `format_compact!` panics if a formatting trait implementation returns
+/// an error.
+///
+/// This indicates an incorrect implementation since
+/// [`ToCompactString::to_compact_string`] never returns an error itself.
 #[macro_export]
 macro_rules! format_compact {
     ($fmt:expr) => {{ $crate::ToCompactString::to_compact_string(&$fmt) }};

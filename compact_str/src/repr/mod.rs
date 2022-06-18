00683c19fa6249d514783b3da0a8d9761289a30e
Parker Timmerman <parker@parkertimmerman.com>
2022-06-17 20:18:15 -0400

    branch start, optimize fmt::Write
    Co-authored-by: René Kijewski <rene.kijewski@fu-berlin.de>
    Co-authored-by: Parker Timmerman <parker@parkertimmerman.com>

--- compact_str/src/repr/mod.rs
+++ compact_str/src/repr/mod.rs
@@ -425,6 +425,16 @@ impl fmt::Write for Repr {
         self.push_str(s);
         Ok(())
     }
+
+    fn write_fmt(mut self: &mut Self, args: fmt::Arguments<'_>) -> fmt::Result {
+        match args.as_str() {
+            Some(s) => {
+                self.push_str(s);
+                Ok(())
+            }
+            None => fmt::write(&mut self, args),
+        }
+    }
 }
 
 #[derive(Debug)]

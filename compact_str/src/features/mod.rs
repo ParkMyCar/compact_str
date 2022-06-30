7550ba77d54113725f68136c86862ea9c03c480d
Parker Timmerman <parker@parkertimmerman.com>
2022-06-30 01:10:51 -0400

    Implement the Arbitrary trait for:   * abritrary   * proptest   * quickcheck
    Add sanity tests to make sure the implementations work, and strings are lined when we expect

--- compact_str/src/features/mod.rs
+++ compact_str/src/features/mod.rs
@@ -1,6 +1,12 @@
 //! A module that contains the implementations for optional features. For example `serde` support
 
+#[cfg(feature = "arbitrary")]
+mod arbitrary;
 #[cfg(feature = "bytes")]
 mod bytes;
+#[cfg(feature = "proptest")]
+mod proptest;
+#[cfg(feature = "quickcheck")]
+mod quickcheck;
 #[cfg(feature = "serde")]
 mod serde;

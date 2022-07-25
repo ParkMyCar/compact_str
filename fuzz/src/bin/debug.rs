d7673f9022156bb32bbc02ad474955fbd043a39a
Parker Timmerman <parker@parkertimmerman.com>
2022-07-24 20:36:14 -0400

    fix: Test case discovered by AFL (#161)
    * fix: Bug when trying to create a CompactString with capacity of 16711422 on 32-bit arches
    * Fixed the underlying bug, which was related to the change in HEAP_MASK
    * Add debug.rs binary to repro single failing fuzz cases
    * Add unit test with comment to repro the exact failure
    * ignore all capacity values test in miri

---
+++ fuzz/src/bin/debug.rs
@@ -0,0 +1,26 @@
+//! Binary that allows you to pass an input file, it creates a [`compact_str_fuzz::Scenario`] and
+//! then runs it.
+//!
+//! This is helpful when AFL finds failures and we need to reproduce them.
+
+use std::path::PathBuf;
+
+use arbitrary::{
+    Arbitrary,
+    Unstructured,
+};
+use compact_str_fuzz::Scenario;
+
+pub fn main() {
+    let path = std::env::args()
+        .nth(1)
+        .map(PathBuf::from)
+        .expect("no path provided!");
+    let data = std::fs::read(path).expect("failed to read input file");
+    let mut unstructured = Unstructured::new(&data);
+    let scenario = Scenario::arbitrary(&mut unstructured).expect("failed to create Scenario");
+
+    println!("Scenario: {:?}", scenario);
+
+    scenario.run();
+}
